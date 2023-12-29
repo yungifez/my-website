7e3152345b2b0b9ae5f624aa63c5d86f7f45efc4
Marvellous <ifezueyoung@gmail.com>
2023-12-29 13:08:52 -0800

    Added my works

--- src/components/MyWorks.vue
+++ src/components/MyWorks.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-  
+
 </script>
 
 <template>
   <section
-    class="mt-5 p-5 leading-7 animate-on-scroll opacity-0"
+    class="mt-5 leading-7 animate-on-scroll opacity-0"
     data-animate-scroll-class="animate-in-pulse"
   >
     <div class="w-fit m-auto">
@@ -16,7 +16,7 @@
       </h2>
     </div>
 
-    <div class="mt-5 p-3 bg-white bg-opacity-10 backdrop-blur">
+    <div class="mt-5 p-6 bg-white bg-opacity-10 backdrop-blur-sm">
       <p class="flex flex-col md:flex-row items-start">
         <span>
           <span class="text-green-300">marvellous@arch.btw</span>
@@ -36,8 +36,31 @@
           Compiling data................
         </span>
       </p>
-      <div class="animate-on-scroll" data-animate-scroll-class="animate-pulse" style="animation-delay: 2s ">
-
+      <div class="animate-on-scroll mt-3" data-animate-scroll-class="animate-in-pulse" id="works">
+        <div>
+          <h3 class="text-3xl my-2"><a class="text-green-500 underline" href="http://github.com/yungifez/skuul">Skuul</a></h3>
+          <p>
+            Skuul is a multi school management system open source project with the aim to streamline the daily activities of schools. It has a lot of features such as student record management, timetable management, syllabus management, notices, exam management, multi school management, etc. 
+          </p>
+        </div> 
+        <div>
+          <h3 class="text-3xl my-2"><a class="text-green-500 underline" href="http://gentletouchschool.com">Gentle Touch Schools</a></h3>
+          <p>
+            I built the website of Gentle Touch Schools, and set up an instance of skuul for the school. This site is built using modern Wordpress toolings by roots.io such as Bedrock and their theme builder called sage.
+          </p>
+        </div> 
+        <div>
+          <h3 class="text-3xl my-2 underline">Jollof Cuisine v2</h3>
+          <p>
+            Jollof Cuisine is a food delivery platform, with features such as location based restaurant suggestions, flexible role management for both in-house and restaurant management, real-time order tracking, flexible menu management system, etc. 
+          </p>
+        </div> 
+        <div>
+          <h3 class="text-3xl my-2 underline">Open Source Contributions</h3>
+          <p>
+          I have contributed to various open sourcve projects such as <a href="http://github.com/4jean/lavsms">LavSMS</a>, <a href="https://github.com/jeroennoten/Laravel-AdminLTE">Laravel AdminLTE</a>, <a href="http://github.com/changeweb/unified-transform"> Unified Transform</a> through code contributions and issue management, did I mention the time I fixed some issues in ``the jetstream and spatieroles and permission docs hehe :). 
+          </p>
+        </div> 
       </div>
     </div>
   </section>
@@ -50,13 +73,18 @@
   #shell-output :first-child{
    animation-delay: 3.5s;
   }
-  
+  #works {
+    animation-delay: 3.8s;
+  }
   @media (min-width: 750px){
     #shell-output span {
       animation-delay: 1.5s;
     }
     #shell-output :first-child{
      animation-delay: 1.3s;
+    }
+    #works {
+      animation-delay: 1.7s;
     }
 
   }
